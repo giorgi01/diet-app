fe9be43203aaf3d0ad77bf50cade0fd79cb0e5f8
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-25 22:01:56 +0400

    Implemented calories_taken, calories_left functionality to Models

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,8 @@
 class Meal < ApplicationRecord
   belongs_to :user
   validates_presence_of :date, :time, :name, :calories
+
+  def self.todays_meals
+    where(date: Date.today)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :meals
   enum role: { user: 0, manager: 1, admin: 2 }
+
+  def calories_taken
+    sum = 0
+    Meal.todays_meals.each { |m| sum += m.calories if m.user_id == self.id }
+    sum
+  end
+
+  def calories_left
+    self.daily_calories - calories_taken
+  end
 end
