a3727e626c6c003310650871bc5c9db9b00b0514
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-26 14:45:44 +0400

    Updated views and stylesheets

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :user
   validates_presence_of :date, :time, :name, :calories
+  validates :calories, numericality: { greater_than_or_equal_to: 0 }
 
   def self.todays_meals
     where(date: Date.today)
